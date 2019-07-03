ea32d3a96222dad2fe5427ae9f81c45cf9f15f5f
reine-marie brel <brelrm@gmail.com>
2019-07-03 13:35:20 +0200

    ajout Monstre et Arme

--- Personnage.cs
+++ Personnage.cs
@@ -3,6 +3,11 @@ class Personnage{
 	Personnage(string n){
 	 this.nom = n;
 	 }
+
 	 public void Frapper( Personnage p, int force){}
 	  public void Deplacer( Direction direction){}
+
+
+	  public Lieu regarder( Direction direction, int distance){}
+
 }
